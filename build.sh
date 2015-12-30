c3177898ad586fb1a4bcbfda00bfde2ad6229e4d
Byteflux <byte@byteflux.net>
2015-12-29 20:40:32 -0800

    Add submodules and build scripts

---
+++ build.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+git submodule update --init && ./remap.sh && ./decompile.sh && ./init.sh && ./applyPatches.sh && mvn clean install
--- rebuildPatches.sh
+++ rebuildPatches.sh
@@ -3,18 +3,19 @@
 PS1="$"
 basedir=`pwd`
 echo "Rebuilding patch files from current fork state..."
+git config core.safecrlf false
 
 function cleanupPatches {
     cd "$1"
     for patch in *.patch; do
+        echo "$patch"
         gitver=$(tail -n 2 $patch | grep -ve "^$" | tail -n 1)
         diffs=$(git diff --staged $patch | grep -E "^(\+|\-)" | grep -Ev "(From [a-z0-9]{32,}|\-\-\- a|\+\+\+ b|.index)")
 
         testver=$(echo "$diffs" | tail -n 2 | grep -ve "^$" | tail -n 1 | grep "$gitver")
         if [ "x$testver" != "x" ]; then
-            diffs=$(echo "$diffs" | head -n -2)
+            diffs=$(echo "$diffs" | sed 'N;$!P;$!D;$d')
         fi
-        
 
         if [ "x$diffs" == "x" ] ; then
             git reset HEAD $patch >/dev/null
@@ -26,15 +27,16 @@ function cleanupPatches {
 function savePatches {
     what=$1
     target=$2
+    echo "Formatting patches for $what..."
     cd "$basedir/$target"
-    git format-patch --no-stat -N -o "$basedir/${what}-Patches/" upstream/upstream
+    git format-patch --no-stat -N -o "$basedir/${what}-Patches/" upstream/upstream >/dev/null
     cd "$basedir"
     git add -A "$basedir/${what}-Patches"
     cleanupPatches "$basedir/${what}-Patches"
     echo "  Patches saved for $what to $what-Patches/"
 }
 if [ "$1" == "clean" ]; then
-	rm -rf *-Patches
+	rm -rf Spigot-*-Patches
 fi
-savePatches Bukkit Spigot-API
-savePatches CraftBukkit Spigot-Server
+savePatches Spigot-API Nozzle-API
+savePatches Spigot-Server Nozzle-Server
