5bc975f561c750174d36a76d05aafd95d0a68ed8
0x277F <0x277F@gmail.com>
2016-01-04 12:18:23 -0700

    Oops

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 basedir=`pwd`
 git submodule update --init
-./remap.sh && ./decompile.sh && ./init.sh && ./applyPatches.sh && mvn clean install
-cp Spigot-API/target/spigot-*-SNAPSHOT.jar $basedir
-cp Spigot-Server/target/spigot-*-SNAPSHOT.jar $basedir
+./remap.sh && ./decompile.sh && ./init.sh && ./applyPatches.sh && mvn -f Spigot/pom.xml clean install
+cp Spigot/Spigot-API/target/spigot-*-SNAPSHOT.jar $basedir
+cp Spigot/Spigot-Server/target/spigot-*-SNAPSHOT.jar $basedir
 cd $basedir
 
