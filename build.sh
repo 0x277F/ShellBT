a62784e732c44cb607ccfe50985824b20bc67184
0x277F <0x277F@gmail.com>
2016-01-04 12:29:11 -0700

    I have no idea how to use git

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 basedir=`pwd`
+git submodule add -f https://hub.spigotmc.org/stash/scm/spigot/spigot.git Spigot
 git submodule update --init
 ./remap.sh && ./decompile.sh && ./init.sh && ./applyPatches.sh && mvn -f Spigot/pom.xml clean install
 cp Spigot/Spigot-API/target/spigot-*-SNAPSHOT.jar $basedir
